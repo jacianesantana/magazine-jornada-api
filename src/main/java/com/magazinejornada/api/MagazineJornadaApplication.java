95e7715318303b689c9ba981f9ae0c5b66eca712
jacianesantana <jaciane_santos93@hotmail.com>
2022-09-05 15:24:03 -0300

    Config heroku

--- src/main/java/com/magazinejornada/api/MagazineJornadaApplication.java
+++ src/main/java/com/magazinejornada/api/MagazineJornadaApplication.java
@@ -3,7 +3,7 @@ package com.magazinejornada.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class MagazineJornadaApplication {
 
 	public static void main(String[] args) {
