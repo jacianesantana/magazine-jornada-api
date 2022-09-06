360b04d6fb44537b27d16d8c518f776f94469e8f
Bruuno-Alves <bruno.a.santos@live.com>
2022-09-06 18:00:07 -0300

    add product

---
+++ src/main/java/com/magazinejornada/api/model/Product.java
@@ -0,0 +1,25 @@
+package com.magazinejornada.api.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "productTable")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Long quantity;
+    private Double price;
+    private String picture;
+}
---
+++ src/main/java/com/magazinejornada/api/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.magazinejornada.api.repository;
+
+import com.magazinejornada.api.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
